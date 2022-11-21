d20720a9b55699f4003ee4f08e1bf0b309771f4d
RubenPPP <aluno20966@ipt.pt>
2022-11-21 16:55:49 +0000

    Removed Constructors and Redundant Variables

--- CarsOnBlocks/src/MainPackage/Client.java
+++ CarsOnBlocks/src/MainPackage/Client.java
@@ -17,10 +17,7 @@ import java.security.KeyPair;
  * @author Rodrigo Maia & Rúben Poupado
  */
 public class Client {
-    private String name;
-    private String birthdate;
-    private String NIF;
-    
+    private String username;
     private String password;
     private Key privKey;
     private Key pubKey;
@@ -28,45 +25,8 @@ public class Client {
     boolean isLogged = false;
     boolean alreadyExists = false;
     
-    public Client(){
-        this.name = "Henrico Chivaldori";
-        this.birthdate = "16/02/1972";
-        this.NIF = "#adA#ouAWEU34auoSD";
-    }
-    
-    public Client(String name, String NIF){
-        this.name = name;
-        this.NIF = NIF;
-    }
-    
-    public Client(String name, String NIF, String birthdate){
-        this.name = name;
-        this.NIF = NIF;
-        this.birthdate = birthdate;
-    }
-    
     public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getBirthdate() {
-        return birthdate;
-    }
-
-    public void setBirthdate(String birthdate) {
-        this.birthdate = birthdate;
-    }
-
-    public String getNIF() {
-        return NIF;
-    }
-
-    public void setNIF(String NIF) {
-        this.NIF = NIF;
+        return username;
     }
         
     public void register(String username, String password) throws Exception{
